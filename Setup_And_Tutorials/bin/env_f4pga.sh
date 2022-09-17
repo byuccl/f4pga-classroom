5a6bb09522ff2cb6907381cfdcda4b8ecc440550
westonMS <97260484+westonMS@users.noreply.github.com>
2022-09-17 17:33:54 -0600

    Update env_f4pga.sh

--- Setup_And_Tutorials/bin/env_f4pga.sh
+++ Setup_And_Tutorials/bin/env_f4pga.sh
@@ -1,7 +1,6 @@
 export F4PGA_EXAMPLES_ROOT=/opt/f4pga
-export INSTALL_DIR=$F4PGA_EXAMPLES_ROOT/opt/f4pga
+export F4PGA_INSTALL_DIR=/opt/f4pga
 FPGA_FAM="xc7"
-export PATH="$INSTALL_DIR/$FPGA_FAM/install/bin:$PATH";
-source "$INSTALL_DIR/$FPGA_FAM/conda/etc/profile.d/conda.sh"
+export PATH="$F4PGA_INSTALL_DIR/$FPGA_FAM/install/bin:$PATH";
+source "$F4PGA_INSTALL_DIR/$FPGA_FAM/conda/etc/profile.d/conda.sh"
 conda activate $FPGA_FAM
-echo "Environment activated"
